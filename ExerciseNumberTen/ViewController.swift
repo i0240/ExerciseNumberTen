2c2df98a8689e05b6bfc5302d8a284ead48f0fc7
Ilia <ilya2491@gmail.com>
2023-05-24 10:16:27 +0300

    Initial Commit

---
+++ ExerciseNumberTen/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ExerciseNumberTen
+//
+//  Created by i0240 on 24.05.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
